a1a277663467c4871370c245d1ac31097a91729a
Enrico Cavicchioli <enricocavic@gmail.com>
2024-01-15 21:09:21 -0300

    Update WordFilter.py

--- WordFilter.py
+++ WordFilter.py
@@ -1,13 +1,20 @@
 # Filtro de p***ria no chat
+import os
+
+def clear():
+    os.system('cls')
 
 PATH = 'palavras.txt'
 ENCODING = 'utf-8'
 MAXIMUM_INFRACTIONS = 10
 
+clear()
 with open(PATH,'r', encoding=ENCODING) as file:
     termList = file.read().splitlines()
 
 NICKNAME = input('Entre com seu nickname:')
+clear()
+
 
 def censorText(_textToCensor):
     global current_infractions 
@@ -33,12 +40,14 @@ current_infractions = 0
 while current_infractions < MAXIMUM_INFRACTIONS:
     print('---=//=---' * 5)
     input_text = input(f'[{NICKNAME}]:')
+    clear()
 
     if(input_text == 'exit'):
         break
     
     print(censorText(input_text))
 
+clear()
 print('Você foi banido do chat de texto')
 
 
